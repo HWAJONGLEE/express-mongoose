59ceaccb556f9ca89684abaa1f53de989f5bb096
HWAJONG LEE <wowlover10@nate.com>
2022-02-21 14:14:17 +0900

    npm init

--- fileAsync.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-
-fs.readFile('./hello.js', 'utf-8', function (err, result) {
-    console.log('file read completed..');
-    console.log(result);
-});
-
-console.log('async code...');
--- fileSync.js
+++
@@ -1,9 +0,0 @@
-// node 의 파일명은 카멜케이스
-var fs = require('fs');
-
-var result = fs.readFileSync('./hello.js', 'UTF-8');
-
-console.log('파일을 다 읽어오고 나서 실행되는 라인');
-
-
-console.log(result);
